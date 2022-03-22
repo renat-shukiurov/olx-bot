9fc38a26df30b804606fea3e817431bd9c47712b
Server <renat@server.com>
2022-03-22 12:25:13 +0000

    Revert working version

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const GetAdvertsTable = async (url) => {
     return table;
 
 }
-const getStoredAdvert = async (url) => {
+const isStoredAdvert = async (url) => {
     const client = await MongoClient.connect(mongoConnUrl, { useNewUrlParser: true })
         .catch(err => { console.log(err); });
 
@@ -34,7 +34,7 @@ const getStoredAdvert = async (url) => {
     try {
         const db = client.db("olx-bot");
         const collection = db.collection("adverts");
-        return collection.findOne({url})
+        return await collection.findOne({url})
     } catch (err) {
         console.log(err);
     } finally {
@@ -107,13 +107,11 @@ const run = () => {
                     if(!linkItem) continue;
 
                     const advert = getAdvertObj(rows[i]);
-                    getStoredAdvert(advert.href).then(async res => {
-                        if (!res) {
-                            const msg = `Нове оголошення у місті <b>${advert.city}</b>\n<a href="${advert.href}">${advert.title}</a> \n\nЦіна <b>${advert.price}</b> \n\n<a href="${searchUrl}">Збережений пошук</a>`;
-                            await sendNotification(msg, advert.img);
-                            await saveAdvert(searchUrl, advert.href);
-                        }
-                    })
+                    if (! await isStoredAdvert(advert.href)) {
+                        const msg = `Нове оголошення у місті <b>${advert.city}</b>\n<a href="${advert.href}">${advert.title}</a> \n\nЦіна <b>${advert.price}</b> \n\n<a href="${searchUrl}">Збережений пошук</a>`;
+                        await sendNotification(msg, advert.img);
+                        await saveAdvert(searchUrl, advert.href);
+                    }
                 }
             })
     }
@@ -121,7 +119,3 @@ const run = () => {
 
 run();
 setInterval(() => run(), 60000);
-
-
-
-
